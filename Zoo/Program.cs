bb5e1e726d3eb5e0191a0a2f83ccc655d38834c3
Kozak <Kozak@AABFB9>
2019-11-24 13:50:48 -0800

    Update class Program

--- Zoo/Program.cs
+++ Zoo/Program.cs
@@ -4,9 +4,38 @@ namespace Zoo
 {
     class Program
     {
-        static void Main(string[] args)
+        private Bear bear = new Bear();
+        public string getUserInput()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("================");
+            Console.WriteLine("Zoo Menu Planner");
+            Console.WriteLine("================");
+            Console.WriteLine("1. Monkey");
+            Console.WriteLine("2. Bear");
+            Console.WriteLine("3. Quit");
+            Console.Write("Selection: \n");
+            string input = Console.ReadLine();
+            if (input == "1") {
+                // this.monkey.GetMonkey();
+            }
+            else if (input == "2") {
+                this.bear.GetBear();
+            }
+            else if (input == "3") {
+                Console.WriteLine("To exit, press ENTER one more time. \n");
+            }
+            else {
+                Console.WriteLine("Invalid input!");
+            }
+            return input;
+        }
+        static void Main(string[] args) {
+ 
         }
     }
 }
+
+
+
+
+
