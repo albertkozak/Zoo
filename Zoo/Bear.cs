47a2d3b93ede5bef9caba0e3214a7fdae98e7677
Kozak <Kozak@AABFB9>
2019-11-24 12:55:55 -0800

    class Bear

---
+++ Zoo/Bear.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    class Bear : Mammal
+    {
+        const double BB_SERVE_TO_WEIGHT_RATIO = 0.014;
+        const double PB_SERVE_TO_WEIGHT_RATIO = 0.016;
+        public void GetBear() {
+            Console.WriteLine("Species");
+            Console.WriteLine("--------");
+            Console.WriteLine("1. Black");
+            Console.WriteLine("2. Polar");
+            Console.WriteLine("Selection \n");
+            string input = Console.ReadLine();
+            string feedTime = "9AM and 3PM";
+            if (input == "1") {
+                double servingToWeightRatio = BB_SERVE_TO_WEIGHT_RATIO;
+                double mammalWeight = base.GetMammalWeight();
+            }
+            else if (input == "2") {
+                double servingToWeightRatio = PB_SERVE_TO_WEIGHT_RATIO;
+                double mammalWeight = base.GetMammalWeight();
+            }
+            else {
+                Console.WriteLine("Invalid input!");
+                this.GetBear();
+            }
+        }
+    }
+}
---
+++ Zoo/Mammal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    class Mammal
+    {
+        public double GetMammalWeight() {
+            double mammalWeight;
+            Console.WriteLine("Weight in KG: \n");
+            double input = 0;
+            try {
+                input = Convert.ToDouble(Console.ReadLine());
+            }
+            catch {
+                Console.WriteLine("Invalid input for weight! \n");
+                GetMammalWeight();
+            }
+            if (input < 0) {
+                Console.WriteLine("Weight cannot be zero! \n");
+                this.GetMammalWeight();
+            }
+            else {
+                mammalWeight = input;
+            }
+            return input;
+        }
+    }
+}
